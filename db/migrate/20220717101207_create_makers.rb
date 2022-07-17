c2e4a89c83afd5dd7cf2b4a8c2c61c4351d966af
Syaz0326 <tadakazu326@gmail.com>
2022-07-17 19:12:25 +0900

    Add create Maker migrate

---
+++ db/migrate/20220717101207_create_makers.rb
@@ -0,0 +1,10 @@
+class CreateMakers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :makers do |t|
+      t.string :name
+      t.string :description
+
+      t.timestamps
+    end
+  end
+end
