0ac9473b87cb3b0dc35caee2d5913ade3c49d0bf
Syaz0326 <tadakazu326@gmail.com>
2022-07-20 00:02:12 +0900

    Add review tags form

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -14,15 +14,44 @@ class ReviewsController < ApplicationController
       render 'index'
     end
 
-    @review = Review.new(review_params.merge(product_id: product_id))
+    review = {
+      review: {
+        product_id: product_id,
+        comment: review_params[:comment],
+        reviewer_name: review_params[:reviewer_name],
+        price_score: review_params[:price_score],
+        design_score: review_params[:design_score],
+        quality_score: review_params[:quality_score],
+      },
+      tag: {}
+    }
+    review_tags = ReviewTag.all
+    review_tags.select(:name).each do |tag|
+      review[:tag][tag.name] = review_params[tag.name]
+    end
+
+    @review = Review.new(review[:review])
     @review.save
+    review[:tag].each do |tag_name, value|
+      if value == '1'
+        review_tag_id = review_tags.find_by(name: tag_name).id
+        ReviewTagMap.create(review_id: @review.id,
+                           review_tag_id: review_tag_id)
+      end
+    end
     redirect_to reviews_path(@product)
   end
 
   def review_params
+    review_tags = ReviewTag.select(:name).map do |tag|
+      tag.name
+    end
+
     params.
       require(:review)
       .permit(:comment, :reviewer_name,
-              :price_score, :design_score, :quality_score)
+              :price_score, :design_score, :quality_score,
+              *review_tags
+             )
   end
 end
