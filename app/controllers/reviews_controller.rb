dd2f345ec5bba70f834de2d3c14967d9ba3ef2ab
Syaz0326 <tadakazu326@gmail.com>
2022-07-19 23:00:00 +0900

    Add review form

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,9 +1,28 @@
 class ReviewsController < ApplicationController
-  def new
-  end
-
   def index
     @product = Product.find_by(id: params[:id])
     @reviews = @product.review
+
+    @review_tags = ReviewTag.all
+    @new_review = Review.new()
+  end
+
+  def create
+    @product = Product.find_by(id: params[:id])
+    product_id = @product.id
+    if product_id == nil
+      render 'index'
+    end
+
+    @review = Review.new(review_params.merge(product_id: product_id))
+    @review.save
+    redirect_to reviews_path(@product)
+  end
+
+  def review_params
+    params.
+      require(:review)
+      .permit(:comment, :reviewer_name,
+              :price_score, :design_score, :quality_score)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   get '/items', to: 'products#index', as: 'items'
   get '/items/:id', to: 'products#show', as: 'item'
   get '/items/:id/reviews', to: 'reviews#index', as: 'reviews'
+  post '/items/:id/reviews', to: 'reviews#create', as: 'post_review'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
