8dfedcc6f433b69404a901f4912bd7878bba65d7
Syaz0326 <tadakazu326@gmail.com>
2022-07-21 22:56:23 +0900

    Add change products sort

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,13 @@
 class ProductsController < ApplicationController
   def index
-    @items = Product.all.sort_by do |product|
-      product.review.size
-    end
-      .reverse
+    @sort_items = {
+      レビュー数: 'review_count',
+      評価順: 'review_score',
+      新着順: 'recent'
+    }
+    @sort_by = params[:sort_by]
+    products = Product.all
+    @items = sort_by(products, @sort_by)
   end
 
   def show
@@ -23,4 +27,42 @@ class ProductsController < ApplicationController
 
     @other_products = Product.where(maker_id: item.maker_id)
   end
+
+  public
+    def sort_by(products, by)
+      case by
+      when 'review_count' then
+        return sort_by_review_count(products)
+      when 'review_score' then
+        return sort_by_score(products)
+      when 'recent' then
+        return sort_by_recent(products)
+      else
+        return sort_by_review_count(products)
+      end
+    end
+
+    def sort_by_review_count(products)
+      products.sort_by do |product|
+        product.review.size
+      end
+        .reverse
+    end
+
+    def sort_by_score(products)
+      products.sort_by do |product|
+        total_score(product.review)
+      end
+        .reverse
+    end
+
+    def sort_by_recent(products)
+      products.order(:created_at)
+    end
+
+    def sort_params
+      params.permit(:sort_by)
+    end
+
+  include ProductsHelper
 end
