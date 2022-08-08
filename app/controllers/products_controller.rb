f91573f85caf2e0576ee332b27e14bce99e805d3
Syaz0326 <tadakazu326@gmail.com>
2022-08-08 10:54:56 +0900

    Change ProductsController sort methods to private

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -14,45 +14,46 @@ class ProductsController < ApplicationController
     @item = Product.find_by(id: params[:id])
 
     @other_products = Product
-      .where(maker_id: @item.maker_id)
-      .where.not(id: params[:id])
+                      .where(maker_id: @item.maker_id)
+                      .where.not(id: params[:id])
   end
 
-  public
-    def sort_by(products, by)
-      case by
-      when 'review_count' then
-        return sort_by_review_count(products)
-      when 'review_score' then
-        return sort_by_score(products)
-      when 'recent' then
-        return sort_by_recent(products)
-      else
-        return sort_by_review_count(products)
-      end
-    end
+  private
 
-    def sort_by_review_count(products)
-      products.sort_by do |product|
-        product.review.size
-      end
-        .reverse
+  def sort_by(products, by)
+    case by
+    when 'review_count'
+      sort_by_review_count(products)
+    when 'review_score'
+      sort_by_score(products)
+    when 'recent'
+      sort_by_recent(products)
+    else
+      sort_by_review_count(products)
     end
+  end
 
-    def sort_by_score(products)
-      products.sort_by do |product|
-        total_score(product.review)
-      end
-        .reverse
+  def sort_by_review_count(products)
+    products.sort_by do |product|
+      product.review.size
     end
+            .reverse
+  end
 
-    def sort_by_recent(products)
-      products.order(:created_at)
+  def sort_by_score(products)
+    products.sort_by do |product|
+      total_score(product.review)
     end
+            .reverse
+  end
 
-    def sort_params
-      params.permit(:sort_by)
-    end
+  def sort_by_recent(products)
+    products.order(:created_at)
+  end
+
+  def sort_params
+    params.permit(:sort_by)
+  end
 
   include ProductsHelper
 end
